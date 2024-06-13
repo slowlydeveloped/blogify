3f46862b4cd29a01b0d188bd4ec5b3fd7a553865
Jitesh Yadav <yadavjitesh939@gmail.com>
2024-06-13 16:02:10 +0530

    Add app_secrets.dart to .gitignore and remove from repository

--- lib/core/secrets/app_secrets.dart
+++
@@ -1,4 +0,0 @@
-class AppSecrets {
-  static const supabaseUrl = 'https://xliolittjxhhfsgjqsos.supabase.co';
-  static const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhsaW9saXR0anhoaGZzZ2pxc29zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTc3NDUwMzEsImV4cCI6MjAzMzMyMTAzMX0.eGAymynvHD8l0FAt-T5uvrfPt8w6ucEjz5BdCbw0iLA';
-}
